8810f8cb1bc4f50d5e2d87dc5ccae90f45daf588
PeterDeNardo <34484758+PeterDeNardo@users.noreply.github.com>
2018-10-26 20:58:09 -0300

    Eliptico

---
+++ Eliptico.py
@@ -0,0 +1,15 @@
+from visual import *
+
+a2 = 0.0
+
+b2 = sphere(radius = 0.3, pos = (0, 0, 3.4),
+            color = color.yellow, material = materials.emissive, make_trail = True)
+
+l2 = local_light(pos = b2.pos, color = b2.color)
+
+while True:
+    rate(100)
+
+    l2.pos = b2.pos = (3.4) * vector(0,sin(a2), sin(a2) - cos(a2))
+    a2 += 0.055
+    
