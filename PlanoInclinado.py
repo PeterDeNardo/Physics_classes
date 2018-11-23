349a53a8bf2546b68d5677a4de4678a969b8774f
PeterDeNardo <34484758+PeterDeNardo@users.noreply.github.com>
2018-11-23 10:32:10 -0200

    Add files via upload

--- PlanoInclinado.py
+++ PlanoInclinado.py
@@ -1,28 +1,30 @@
-from visual import *
+from vpython import *
 import math
 
-angulo = 45.0 
+angulo = 60
 
-floor = box(length=100, height=0.1, width=10, axis=vector(math.cos(angulo), math.sin(angulo) ,0), color=color.blue)
+floor = box(length=100, height=0.1, width=10, axis=vector(math.cos(math.radians(angulo)), math.sin(math.radians(angulo)), 0), color=color.white)
 
-ball = sphere(pos=(0,0.5,0), color=color.red)
-ball.velocity = vector(0,0,0)
-m = 2.0
+ball = sphere(pos= vector(0, 0.5, 0), color=color.red)
+ball.velocity = vector(0, 0, 0)
+m = 0.2
 g = 1.0
-p = m*g
+p = m * g
 
 dt = 0.05
+
+px = 0
+py = 0
+
 while 1:
-    rate(100)
+    rate(10)
 
-    ax = g * math.sin(angulo)
-    ay = g * math.cos(angulo)
-    
-    px = p * math.sin(angulo)
-    py = p * math.cos(angulo)
-    
-    ball.velocity.y = -py * ay 
-    ball.velocity.x = -px * ax
+    px += p * math.cos(math.radians(angulo))
+    py += p * math.sin(math.radians(angulo))
 
-    ball.pos +=  ball.velocity*dt
-    
+    ball.velocity.y = -py
+    ball.velocity.x = -px
+
+    print(ball.velocity)
+
+    ball.pos += ball.velocity * dt
